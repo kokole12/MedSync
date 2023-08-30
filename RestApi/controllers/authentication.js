61b3ef81d61f669b34c73d421a19fa2402b23c61
kokole12 <kokoleismail886@gmail.com>
2023-08-30 20:19:18 +0300

    ll

--- RestApi/controllers/authentication.js
+++ RestApi/controllers/authentication.js
@@ -1,7 +1,15 @@
 const User = require('../models/user');
+const bctypt = require('bcryptjs');
+const {StatusCodes} = require('http-status-codes');
 
 const register = async(req, res) => {
-    res.send('register route');
+    const {email, username, passord, role} = req.body
+    const salt = bctypt.genSalt(10);
+    const hashedPassword = await bctypt.hash(passord, salt);
+
+    const tempUser = {email, username, passord: hashedPassword, role};
+    const user = await User.create({...tempUser});
+    res.status(StatusCodes.CREATED).json({user});
 }
 
 const login = async(req, res) => {
