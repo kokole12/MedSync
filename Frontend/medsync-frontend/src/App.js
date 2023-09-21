f74f90191db0c6c81bb4830388291bc912d39fff
Client <client@Clients-MacBook-Pro.local>
2023-09-21 11:39:00 +0100

    my commit

--- Frontend/medsync-frontend/src/App.js
+++ Frontend/medsync-frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       {/* <Landing /> */}
-      <Doctors />
+      {/* <Doctors /> */}
       {/* <Register /> */}
       {/* <Login /> */}
     </div>
---
+++ Frontend/medsync-frontend/src/Components/Doctors/ListOfDoctor.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListOfDoctor = () => {
+  return (
+    <div>ListOfDoctor</div>
+  )
+}
+
+export default ListOfDoctor
